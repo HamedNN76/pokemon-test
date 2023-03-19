922ca8c72d6b5d7a8833789b6d51fffd4f239380
HamedNN76 <hamed.navabian@gmail.com>
2023-03-20 01:51:51 +0330

    nextjs package renamed to pokedex

--- packages/pokedex/src/pages/_app.tsx
+++ packages/pokedex/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@pokemon/styles/globals.css'
+import '@pokedex/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
