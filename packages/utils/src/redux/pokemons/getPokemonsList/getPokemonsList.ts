3cd16e9a9f8b8e4acc27c428c518b0295c1f4175
HamedNN76 <hamed.navabian@gmail.com>
2023-03-27 07:31:11 +0330

    - utils: -- getPokemonsList pagination added

--- packages/utils/src/redux/pokemons/getPokemonsList/getPokemonsList.ts
+++ packages/utils/src/redux/pokemons/getPokemonsList/getPokemonsList.ts
@@ -2,14 +2,18 @@ import {ReduxFetchState} from 'redux-fetch-state';
 import {put, takeEvery} from 'redux-saga/effects';
 import {ListEndpointOptions, PokedexApi} from '../../../pokedex';
 import Pokedex from 'pokedex-promise-v2';
+import {TGetPokemonsListAction} from '../pokemon.type';
 
 export const GetPokemonsFetchState = new ReduxFetchState<Pokedex.NamedAPIResourceList, ListEndpointOptions, string>(
   'getPokemons',
 );
 
-export function* watchGetPokemons() {
+export function* watchGetPokemons(action?: TGetPokemonsListAction) {
+  const {payload} = action || {};
+  const {limit, offset} = payload || {};
+
   try {
-    const data = yield PokedexApi.getPokemonsList();
+    const data = yield PokedexApi.getPokemonsList({limit, offset});
     yield put(getPokemonsActions.loadSuccess(data));
   } catch (e) {
     yield put(getPokemonsActions.loadFailure(e.message));
--- packages/utils/src/redux/pokemons/pokemon.type.ts
+++ packages/utils/src/redux/pokemons/pokemon.type.ts
@@ -1,4 +1,12 @@
 import PokeAPI from 'pokedex-promise-v2';
+import {ListEndpointOptions} from '../../pokedex';
+
+export type TGetPokemonsListRes = PokeAPI.NamedAPIResourceList;
+
+export type TGetPokemonsListAction = {
+  type: string;
+  payload: ListEndpointOptions;
+};
 
 export type TGetPokemonByNameRes = PokeAPI.Pokemon;
 
